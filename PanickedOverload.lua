cde6c3944f9a372b2faf62392836adec20ddfd6f
Memerguy <54774419+Memerguyy@users.noreply.github.com>
2025-04-10 19:39:22 +0200

    Hotfix
    Formatting fix + a slight fucky wucky

--- PanickedOverload.lua
+++ PanickedOverload.lua
@@ -14,7 +14,7 @@ SMODS.Joker {
 	key = 'PanickedOverload',
 	loc_txt = {
 		name = 'Panicked Overload',
-		text = {"Because what else is more fun than 5 cards?","Correct! a whole whoppin {S:1.2. X:attention,C:white}SIX{} of em!","Allows you to {C:chips}select{}","{C:white,X:mult}6{} cards instead of {C:white,X:mult}5{} on your last hand."},
+		text = {"What else can be more fun than 5 cards?","Correct! a whole whoppin {C:white,X:green}SIX!{} of em!","Allows you to {C:chips}select{} {C:white,X:mult}6{} cards","instead of {C:white,X:mult}5{} on your last {C:chips}hand{}."},
 	},
 		discovered = true,
 		unlocked = true,
@@ -29,7 +29,7 @@ SMODS.Joker {
 		calculate = function(self, card, context)
 			--THIS CODE IS **REALLY** FUCKING MESSY, HELD TOGETHER BY DUCT TAPE, if you're here trying to learn something PLEASE be aware that this was put together in (a collective total of) AN HOUR, there's still probably some weird obscure interaction I didn't think off, but oh well :3
 			if context.joker_main then
-				sendTraceMessage(G.play.config.card_limit, "PanickedOverload")
+				sendTraceMessage(G.hand.config.highlighted_limit, "PanickedOverload")
 				if (G.GAME.current_round.hands_left == 1) then
 					G.hand.config.highlighted_limit = 6
 					G.FUNCS.can_play = function(e)
@@ -43,6 +43,15 @@ SMODS.Joker {
 					  end
 				else
 					G.hand.config.highlighted_limit = 5
+					G.FUNCS.can_play = function(e)
+						if #G.hand.highlighted <= 0 or G.GAME.blind.block_play or #G.hand.highlighted > G.hand.config.highlighted_limit then 
+							e.config.colour = G.C.UI.BACKGROUND_INACTIVE
+							e.config.button = nil
+						else
+							e.config.colour = G.C.BLUE
+							e.config.button = 'play_cards_from_highlighted'
+						end
+					  end
 				end
 			end
 		end
