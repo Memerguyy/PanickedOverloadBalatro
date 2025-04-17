3cdaf8ee2aaeae0cb27748dc25862da3f97181f9
Memerguy <54774419+Memerguyy@users.noreply.github.com>
2025-04-17 13:35:06 +0200

    1.0.1 version, slight fixes
    cleaned up code, added a bit more functionality (additionally works like Splash! now)

--- PanickedOverload.lua
+++ PanickedOverload.lua
@@ -14,7 +14,7 @@ SMODS.Joker {
 	key = 'PanickedOverload',
 	loc_txt = {
 		name = 'Panicked Overload',
-		text = {"What else can be more fun than 5 cards?","Correct! a whole whoppin {C:white,X:green}SIX!{} of em!","Allows you to {C:chips}select{} {C:white,X:mult}6{} cards","instead of {C:white,X:mult}5{} on your last {C:chips}hand{}."},
+		text = {"What else can be more fun than 5 cards?","Correct! a whole whoppin {C:white,X:green}SIX!{} of em!","Allows you to {C:chips}select{} {C:white,X:mult}6{} cards","instead of {C:white,X:mult}5{} on your last {C:chips}hand{}.","Every card {C:chips}played{} becomes a {C:white,X:attention}scoring{} card."},
 	},
 		discovered = true,
 		unlocked = true,
@@ -27,32 +27,65 @@ SMODS.Joker {
 		rarity = 2,
 		cost = 5,
 		calculate = function(self, card, context)
-			--THIS CODE IS **REALLY** FUCKING MESSY, HELD TOGETHER BY DUCT TAPE, if you're here trying to learn something PLEASE be aware that this was put together in (a collective total of) AN HOUR, there's still probably some weird obscure interaction I didn't think off, but oh well :3
-			if context.joker_main then
-				sendTraceMessage(G.hand.config.highlighted_limit, "PanickedOverload")
-				if (G.GAME.current_round.hands_left == 1) then
-					G.hand.config.highlighted_limit = 6
-					G.FUNCS.can_play = function(e)
-						if #G.hand.highlighted <= 0 or G.GAME.blind.block_play or #G.hand.highlighted > G.hand.config.highlighted_limit then 
-							e.config.colour = G.C.UI.BACKGROUND_INACTIVE
-							e.config.button = nil
-						else
-							e.config.colour = G.C.BLUE
-							e.config.button = 'play_cards_from_highlighted'
-						end
-					  end
-				else
-					G.hand.config.highlighted_limit = 5
-					G.FUNCS.can_play = function(e)
-						if #G.hand.highlighted <= 0 or G.GAME.blind.block_play or #G.hand.highlighted > G.hand.config.highlighted_limit then 
-							e.config.colour = G.C.UI.BACKGROUND_INACTIVE
-							e.config.button = nil
-						else
-							e.config.colour = G.C.BLUE
-							e.config.button = 'play_cards_from_highlighted'
-						end
-					  end
-				end
+			if context.modify_scoring_hand and G.GAME.current_round.hands_left < 2 then
+			  return {
+				add_to_hand = true
+			  }
 			end
+			if context.joker_main and G.GAME.current_round.hands_left < 2 then
+				G.hand.config.highlighted_limit = 6
+				G.FUNCS.can_play = function(e)
+					if #G.hand.highlighted <= 0 or G.GAME.blind.block_play or #G.hand.highlighted > G.hand.config.highlighted_limit then
+						e.config.colour = G.C.UI.BACKGROUND_INACTIVE
+						e.config.button = nil
+					else
+						e.config.colour = G.C.BLUE
+						e.config.button = 'play_cards_from_highlighted'
+					end
+				end
+			elseif context.joker_main and G.GAME.current_round.hands_left > 2 then
+				G.hand.config.highlighted_limit = 6
+			end
+			if context.end_of_round and context.cardarea == G.jokers then
+					sendDebugMessage("EOR", "PanickedOverload")
+					G.hand.config.highlighted_limit = 5
+			end
+			
+
+			-- OLD CODE, LEAVING IT HERE FOR DISCOVERY PURPOSES
+
+			-- if context.joker_main and G.GAME.current_round.hands_left < 2 then
+			-- 	sendDebugMessage("less than 2 hands left", "PanickedOverload")
+			-- 	G.hand.config.highlighted_limit = 6
+			-- 	G.FUNCS.can_play = function(e)
+			-- 		if #G.hand.highlighted <= 0 or G.GAME.blind.block_play or #G.hand.highlighted > G.hand.config.highlighted_limit then
+			-- 			e.config.colour = G.C.UI.BACKGROUND_INACTIVE
+			-- 			e.config.button = nil
+			-- 		else
+			-- 			e.config.colour = G.C.BLUE
+			-- 			e.config.button = 'play_cards_from_highlighted'
+			-- 		end
+			-- 	end
+			-- elseif context.joker_main and G.GAME.current_round.hands_left > 2 then
+			-- 	G.hand.config.highlighted_limit = 5
+			-- 	G.FUNCS.can_play = function(e)
+			-- 		if #G.hand.highlighted <= 0 or G.GAME.blind.block_play or #G.hand.highlighted > 5 then
+			-- 			e.config.colour = G.C.UI.BACKGROUND_INACTIVE
+			-- 			e.config.button = nil
+			-- 		else
+			-- 			e.config.colour = G.C.BLUE
+			-- 			e.config.button = 'play_cards_from_highlighted'
+			-- 		end
+			-- 	end
+			-- end
+			-- if context.end_of_round and context.cardarea == G.jokers then
+			-- 	sendDebugMessage("EOR", "PanickedOverload")
+			-- 	G.hand.config.highlighted_limit = 5
+			-- end
+			-- if context.before and context.cardarea then
+			-- 	return {
+			-- 		add_to_hand = true
+			-- 	}
+			-- end
 		end
 }
